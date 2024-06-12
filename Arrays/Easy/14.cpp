39198f0b89ea6c06ee1d143e31ca136712aef7cf
SanchitPandey26 <sanchitpandey263@gmail.com>
2024-06-12 10:13:40 +0530

    Solution

--- Arrays/Easy/14.cpp
+++ Arrays/Easy/14.cpp
@@ -2,16 +2,30 @@
 using namespace std;
 
 int main(){
-    int n;
+    int n, k;
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++){
         cin >> arr[i];
     }
-    int num = 0;
+    cin >> k;
+    int maxLen = 0, sum = 0;
+    map<int,int> mpp;
     for (int i = 0; i < n; i++){
-        num = num ^ arr[i];
+        sum += arr[i];
+        if (sum == k){
+            maxLen = max(maxLen, i+1);
+        }
+        int rem = sum - k;
+        if (mpp.find(rem) != mpp.end()){
+            int len = i - mpp[rem];
+            maxLen = max(maxLen, len);
+        }
+        if (mpp.find(sum) == mpp.end()){
+            mpp[sum] = i;
+        }
     }
-    cout << num;
+    cout << maxLen;
+
 
 }
